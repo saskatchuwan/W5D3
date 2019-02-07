7877250ff9b58d3e505cfe0cdad6e7c935f4b91c
username <email>
2019-02-06 17:06:28 -0800

    Add Rails Lite Project

---
+++ skeleton/lib/session.rb
@@ -0,0 +1,26 @@
+require 'json'
+
+class Session
+  # find the cookie for this app
+  # deserialize the cookie into a hash
+  # attr_reader :cookie
+
+  def initialize(req)
+     @cookie ? @cookie = JSON.parse(req.cookies['_rails_lite_app']) : @cookie = {}
+  end
+
+  def [](key)
+   @cookie[key]
+  end
+  
+  def []=(key, val) 
+    @cookie[key] = val
+  end
+
+  # serialize the hash into json and save in a cookie
+  # add to the responses cookies
+  def store_session(res)
+    @cookie = @cookie.to_json
+    res.set_cookie("_rails_lite_app", {path: "/", value: @cookie})
+  end
+end
